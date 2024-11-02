2c615403c6ff369d0d982ed84163de248a7b5824
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2024-11-02 22:39:09 +0100

    correct namespaces

--- rdf_serializer.py
+++ rdf_serializer.py
@@ -2,7 +2,7 @@ import re
 from rdflib import Graph, URIRef, Literal
 from rdflib.namespace import RDF
 
-def generate_class_instance_uri(uri_template, class_instance) -> URIRef:
+def generate_class_instance_uri(uri_template, class_instance, g) -> URIRef:
     """
     This function generates a URI for a class instance based on a URI template.
 
@@ -16,8 +16,23 @@ def generate_class_instance_uri(uri_template, class_instance) -> URIRef:
     # uri_template = class_spec['uri_template']
     variables = re.findall(r'\{(\w+)\}', uri_template)
     uri_values = {var: getattr(class_instance, var) for var in variables}
-    class_uri = URIRef(uri_template.format(**uri_values))
+    class_uri = URIRef_replace_prefix(uri_template.format(**uri_values), g)
     return class_uri
+
+def URIRef_replace_prefix(uri, g) -> str:
+    """
+    This function replaces the prefixes in a URI with their corresponding namespace.
+
+    Args:
+        uri: A URI string.
+
+    Returns:
+        A string with the prefixes replaced by their corresponding namespace.
+    """
+    # replace prefixes with namespaces
+    for prefix, namespace in g.namespaces():
+        uri = uri.replace(prefix + ":", namespace)
+    return URIRef(uri)
 
 def class_to_rdf(class_instance, specification, g=Graph()) -> Graph:
     """
@@ -29,8 +44,6 @@ def class_to_rdf(class_instance, specification, g=Graph()) -> Graph:
     Returns:
         A Graph object containing the RDF triples.
     """
-    print("==============================")
-    print("class_instance: ", class_instance)
     # add all namespaces
     for prefix, uri in specification['namespaces'].items():
         g.bind(prefix, URIRef(uri))
@@ -38,13 +51,13 @@ def class_to_rdf(class_instance, specification, g=Graph()) -> Graph:
     class_name = class_instance.__class__.__name__
     class_spec = specification['classes'][class_name]
 
-    instance_uri = generate_class_instance_uri(class_spec['uri_template'], class_instance)
+    instance_uri = generate_class_instance_uri(class_spec['uri_template'], class_instance, g)
 
     # Check if the instance already exists in the graph
-    if (instance_uri, RDF.type, URIRef(class_spec['rdf_type'])) in g:
+    if (instance_uri, RDF.type, URIRef_replace_prefix(class_spec['rdf_type'], g)) in g:
         return g
     
-    g.add((instance_uri, RDF.type, URIRef(class_spec['rdf_type'])))
+    g.add((instance_uri, RDF.type, URIRef_replace_prefix(class_spec['rdf_type'], g)))
 
     for prop_name, prop_value in class_instance.__dict__.items():
         # skip if variable is None
@@ -53,9 +66,8 @@ def class_to_rdf(class_instance, specification, g=Graph()) -> Graph:
 
         prop_spec = class_spec['properties'].get(prop_name)
         if prop_spec:
-            print("prop_spec: ", prop_spec)
             if prop_spec['is_literal']:
-                g.add((instance_uri, URIRef(prop_spec['predicate']), Literal(prop_value)))
+                g.add((instance_uri, URIRef_replace_prefix(prop_spec['predicate'], g), Literal(prop_value)))
             else:
                 # if it is a list, iterate over each item
                 if isinstance(prop_value, list):
@@ -63,16 +75,16 @@ def class_to_rdf(class_instance, specification, g=Graph()) -> Graph:
                         # if the value is a string, perform mapping
                         if isinstance(item, str):
                             # attempt mapping, if it fails, use original value
-                            item_uri = URIRef(prop_spec['mapping'].get(item, item))
-                            g.add((instance_uri, URIRef(prop_spec['predicate']), item_uri))
+                            item_uri = URIRef_replace_prefix(prop_spec['mapping'].get(item, item), g)
+                            g.add((instance_uri, URIRef_replace_prefix(prop_spec['predicate'], g), item_uri))
                         else:
                             item_class_spec = specification['classes'][item.__class__.__name__]
-                            item_uri = generate_class_instance_uri(item_class_spec['uri_template'], item)
-                            g.add((instance_uri, URIRef(prop_spec['predicate']), item_uri))
+                            item_uri = generate_class_instance_uri(item_class_spec['uri_template'], item, g)
+                            g.add((instance_uri, URIRef_replace_prefix(prop_spec['predicate'], g), item_uri))
                             g = class_to_rdf(item, specification, g)
                 else:
                     item_class_spec = specification['classes'][prop_value.__class__.__name__]
-                    item_uri = generate_class_instance_uri(item_class_spec['uri_template'], prop_value)
-                    g.add((instance_uri, URIRef(prop_spec['predicate']), item_uri))
+                    item_uri = generate_class_instance_uri(item_class_spec['uri_template'], prop_value, g)
+                    g.add((instance_uri, URIRef_replace_prefix(prop_spec['predicate'], g), item_uri))
                     g = class_to_rdf(prop_value, specification, g)
     return g
